7e2d7db0f02b2a03f61bb407426022a03b9de96f
Aman <amanv9617@gmail.com>
2026-04-16 11:35:06 +0530

    add new concept

---
+++ Java_programs/Instance_Members/Non_static_method.java
@@ -0,0 +1,10 @@
+public class Non_static_method {
+        void show(){
+            System.out.println("non static method is running");
+        }
+    public static void main(String[] args) {
+        Non_static_method ma =new Non_static_method();
+        ma.show();
+    }
+}
+

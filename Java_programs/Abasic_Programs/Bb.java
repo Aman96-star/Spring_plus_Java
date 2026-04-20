3f5aef077e44a2e8d835f97ff88dc49745ba0236
Aman <amanv9617@gmail.com>
2026-04-20 12:06:59 +0530

    Make the structure

---
+++ Java_programs/Abasic_Programs/Bb.java
@@ -0,0 +1,12 @@
+public class Bb {
+    
+    
+    public Bb(){
+        System.out.println("zero-param Constructor ");
+    }
+    
+    public static void main(String[] args) {
+        
+        System.out.println("main method");
+    }
+}
